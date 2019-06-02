33213aecfd02c6819f786143e5eae45662450c09
SmileyQuiPleure <merenda.etienne@gmail.com>
2019-06-02 20:56:00 +0200

    I have redo the main's file in Data_scrapper folder. Now, he get alone a name of class information et its value and puts them in dictionary.

--- Data_scrapper/main.py
+++ Data_scrapper/main.py
@@ -1,120 +1,97 @@
-from MySQL_Administrator import MySQLAdministrator
-from BeautifulSoup import Soup
+# conding:utf-8
+
+"""Try alternative method to get information in the page"""
+
+# adding MySQL folder in path for import MySQL_Administrator.
+import sys
+# import Pickle for load and pickle table_name_reference dictionary
+import pickle
+# import pprint for beautiful print
+from pprint import pprint
+# beautifulSoup for parse html text
+from bs4 import BeautifulSoup
+# requests for get page in the net.
 import requests
-import re
-import pprint
+# re for nothing
+
+sys.path.append(sys.path[0]+"\\MySQL")
+# MySQLAdministrator for admin injection in my database.
+from MySQL_Administrator import MySQLAdministrator
+
 
 r = requests.get("https://www.promessedefleurs.com/acorus-gramineus-ogon-jonc-japonais-panache.html")
-soup = Soup(data=r.text)
+soup = BeautifulSoup(r.text, "html5lib")
 
-dictWithoutSearch = {}
-FinalDict = {}
-ErrorDict = {}
+table_name_reference = {"genre": "genre_botanique",
+          "espece": "espece",
+          "famille": "famille",
+          "origine": "origine",
+          "autres_noms": "autres_noms_communs",
+          "couleur": "couleur_fleur",
+          "periode_floraison": "periode_floraison",
+          "inflorescence": "forme_fleur",
+          "taille_fleur": "taille_fleur",
+          "parfum": "parfum",
+          "feuillage": "persistance_feuillage",
+          "couleur_feuillage": "couleur_feuillage",
+          "hauteur": "hauteur",
+          "envergure": "largeur",
+          "croissance": "vitesse_de_pousse",
+          "date_plantation": "periode_raisonnable_plantation",
+          "date_plantation_optimale": "periode_plantation",
+          "type_jardin": "type_jardin",
+          "type_utilisation": "type_utilisation",
+          "rusticite": "rusticite",
+          "densite_plantation": "densite_plantation",
+          "substrat": "specificite_substrat",
+          "exposition": "exposition",
+          "ph_sol": "ph_sol",
+          "drainage": "drainage_humidite",
+          "arrosage": "arrosage",
+          "resistance_maladie": "resistance_maladie",
+          "hivernage": "hivernage",
+          "periode_taille": "periode_taillage",
+          }
+
+
+
+with open("table_name.dic", "wb") as file:
+    pickle.dump(table_name_reference, file)
 
 # name
 
-dictWithoutSearch["nom"] = soup.find("div", attrs={"class": "product-name"})
+name = soup.find("div", attrs={"class": "product-name"}).find("h1").get_text(strip=True)
 
-# Botanique
+# I take the html's part thats interests me
 
-dictWithoutSearch["genre"] = soup.find("li", attrs={"class": "genre_botanique"})
-dictWithoutSearch["espece"] = soup.find("li", attrs={"class": "espece"})
-dictWithoutSearch["famille"] = soup.find("li", attrs={"class": "famille"})
-dictWithoutSearch["origine"] = soup.find("li", attrs={"class": "origine"})
-dictWithoutSearch["autres noms"] = soup.find("li", attrs={"class": "autres_noms_communs"})
+raw_data = {"col2-md": soup.find("div", id="tabs-description").find("div", attrs={"class": "col2-md description"}),
+            "col-right": soup.find("div", id="tabs-soins").find("div", attrs={"class": "col-right"})}
 
-# floraison
+rafined_data = {}
 
-dictWithoutSearch["couleur"] = soup.find("li", attrs={"class": "couleur_fleur"})
-dictWithoutSearch["periode de floraison"] = soup.find("li", attrs={"class": "periode_floraison"})
-dictWithoutSearch["forme fleur"] = soup.find("li", attrs={"class": "forme_fleur"})
-dictWithoutSearch["taille fleur"] = soup.find("li", attrs={"class": "taille_fleur"})
-dictWithoutSearch["parfum"] = soup.find("li", attrs={"class": "parfum"})
+for tag in raw_data.values():
+    for li_tag in tag.find_all("li"):
+        print(li_tag)
+        if li_tag.find("h2") is not None:
+            try:
+                print(li_tag["class"][0], li_tag.find("h2").get_text(strip=True))
+                rafined_data[li_tag["class"][0]] = li_tag.find("h2").get_text(strip=True)
+            except KeyError:
+                print(li_tag["clas"][0], li_tag.find("h2").get_text(strip=True))
+                rafined_data[li_tag["clas"][0]] = li_tag.find("h2").get_text(strip=True)
+        else:
+            try:
+                print(li_tag, li_tag.find("span", class_="value"))
+                rafined_data[li_tag["class"][0]] = li_tag.find("span", class_="value").get_text(strip=True)
+            except KeyError:
+                rafined_data[li_tag["clas"]] = li_tag.find("span", class_="value").get_text(strip=True)
 
-# Feuillage
-dictWithoutSearch["feuillage"] = soup.find("li", attrs={"class": "persistance_feuillage"})
-if dictWithoutSearch["feuillage"] is None:
-    dictWithoutSearch["feuillage"] = soup.find("li", attrs={"clas": "persistance_feuillage"})
-dictWithoutSearch["couleur feuillage"] = soup.find("li", attrs={"class": "couleur_feuillage"})
+# I make list if the data traitement need and .strip() all useless whitespace.
 
-# Port
+for key, value in rafined_data.items():
+    if "," in value:
+        rafined_data[key] = value.split(", ")
+        rafined_data[key] = [i.strip() for i in rafined_data[key]]
 
-dictWithoutSearch["Hauteur à maturité"] = soup.find("li", attrs={"class": "hauteur"})
-dictWithoutSearch["Envergure à maturité"] = soup.find("li", attrs={"class": "largeur"})
-dictWithoutSearch["Croissance"] = soup.find("li", attrs={"class": "vitesse_de_pousse"})
-
-# Date de plantation
-
-dictWithoutSearch["date de plantation"] = soup.find("li", attrs={"class": "periode_raisonnable_plantation"})
-
-# Quel endroit
-
-dictWithoutSearch["type de jardin"] = soup.find("li", attrs={"class": "type_jardin"})
-dictWithoutSearch["type d'utilisation"] = soup.find("li", attrs={"class": "type_utilisation"})
-dictWithoutSearch["rusticité"] = soup.find("li", attrs={"class": "rusticite"})
-# Balise <li> non fermé.------------------Traitement Spécial--------------------
-dictWithoutSearch["densité de plantation"] = soup.find("li", attrs={"class": "densite_plantation"})
-dictWithoutSearch["spécificite du substrat"] = soup.find("li", attrs={"class": "specificite_substrat"})
-dictWithoutSearch["exposition"] = soup.find("li", attrs={"class": "exposition"})
-dictWithoutSearch["ph du sol"] = soup.find("li", attrs={"class": "ph_sol"})
-dictWithoutSearch["drainage humidite"] = soup.find("li", attrs={"class": "drainage_humidite"})
-
-# soins
-
-dictWithoutSearch["arrosage"] = soup.find("li", attrs={"class": "arrosage"})
-dictWithoutSearch["résistance aux maladie"] = soup.find("li", attrs={"class": "resistance_maladie"})
-dictWithoutSearch["hivernage"] = soup.find("li", attrs={"class": "hivernage"})
-dictWithoutSearch["periode de taille"] = soup.find("li", attrs={"class": "periode_taillage"})
-
-for key in dictWithoutSearch.keys():
-    FinalDict[key] = None
-
-try:
-    FinalDict["nom"] = dictWithoutSearch["nom"].find("h1").get_text(" ", strip=True)
-    ErrorDict[FinalDict["nom"]] = {}
-except AttributeError:
-    print("Error Name")
-
-for key, item in dictWithoutSearch.items():
-
-    if FinalDict[key] is None:
-        try:
-            FinalDict[key] = item.h2.text
-        except AttributeError:
-            pass
-
-    if FinalDict[key] is None:
-        try:
-            FinalDict[key] = item.find("span", attrs={"class": "value"}).text
-        except AttributeError:
-            pass
-
-    if FinalDict[key] is None:
-        try:
-            FinalDict[key] = item.find("span", attrs={"class": "value"}).text
-        except AttributeError:
-            pass
-
-    if FinalDict[key] is None:
-        try:
-            FinalDict[key] = item.get_text(" ", strip=True).strip(" ").strip("Exposition")
-            FinalDict[key] = re.sub(" ", "", FinalDict[key])
-            FinalDict[key] = re.sub("\n", " ", FinalDict[key])
-
-        except AttributeError:
-            pass
-
-    if FinalDict[key] is None:
-        print("error", FinalDict[key])
-        try:
-            ErrorDict[FinalDict["nom"]][key] = item
-        except AttributeError:
-            pass
-
-pprint(FinalDict)
-pprint(ErrorDict)
-
-
-# ------------------------------------------------------------------------------
-
-# MySQL Traitement.
+pprint(rafined_data)
+print(len(rafined_data))
